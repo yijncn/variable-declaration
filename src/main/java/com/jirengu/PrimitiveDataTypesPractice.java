d10ca222a73af4221f1e6a8f897dc784c2599c7d
clydeqin7 <clydeqin7@foxmail.com>
2023-08-10 14:27:32 +0800

    feat: 定义及打印变量

--- src/main/java/com/jirengu/PrimitiveDataTypesPractice.java
+++ src/main/java/com/jirengu/PrimitiveDataTypesPractice.java
@@ -5,44 +5,46 @@ public class PrimitiveDataTypesPractice {
         // 先定义变量，然后再修改对应的打印语句按照README中的格式打印变量
 
         //定义变量
+        byte age = 10;
 
         // 修改语句打印变量
-        System.out.println("Age is: ");
+        System.out.println("Age is: " + age);
 
         //定义变量
+        short height = 30000;
 
         // 修改语句打印变量
-        System.out.println("Height is: ");
+        System.out.println("Height is: " + height);
 
         //定义变量
-
+        int num = 1000000000;
         // 修改语句打印变量
-        System.out.println("Num is: ");
+        System.out.println("Num is: " + num);
 
         //定义变量
-
+        long distance = 10000000000L;
         // 修改语句打印变量
-        System.out.println("Distance is: ");
+        System.out.println("Distance is: " + distance);
 
         //定义变量
-
+        float rate = 6/5f;
         // 修改语句打印变量
-        System.out.println("Rate is: ");
+        System.out.println("Rate is: " + rate);
 
         //定义变量
-
+        double fixedRate = 456/123d;
         // 修改语句打印变量
-        System.out.println("Fixed rate is: ");
+        System.out.println("Fixed rate is: " + fixedRate);
 
         //定义变量
-
+        char ch = 'A';
         // 修改语句打印变量
-        System.out.println("Ch: ");
+        System.out.println("Ch: " + ch);
 
         //定义变量
-
+        boolean isUpdated = true;
         // 修改语句打印变量
-        System.out.println("isUpdated: ");
+        System.out.println("isUpdated: " + isUpdated);
         
     }
 }
